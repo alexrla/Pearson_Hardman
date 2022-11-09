e6910a9775f0ed54dac78bd3b338231abb4be44b
Alex Arruda <alexrodrigoarruda@gmail.com>
2022-11-09 17:14:04 -0300

    Adicionada a funcionalidade JavaScript responsável pelo scroll suave

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,6 +2,10 @@ const menuBtn = document.querySelector("#menu");
 const closeMenuBtn = document.querySelector("#close-menu");
 const menu = document.querySelector("#mobile-navbar");
 
+const desktopLinks = document.querySelectorAll("#navbar a");
+const mobileLinks = document.querySelectorAll("#mobile-navbar a");
+const allLinks = [...desktopLinks, ...mobileLinks];
+
 // Menu mobile
 [ menuBtn, closeMenuBtn].forEach((btn) => {
 
@@ -10,3 +14,33 @@ const menu = document.querySelector("#mobile-navbar");
     });
 
 });
+
+// Scroll suave
+function smoothScroll(event) {
+
+    event.preventDefault();
+
+    const href = this.getAttribute("href");
+
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth"
+    });
+
+    setTimeout(() => {
+
+        if(menu.classList.contains("menu-active")) {
+            menu.classList.remove("menu-active");
+        }
+
+    }, 500);
+
+}
+
+allLinks.forEach((link) => {
+
+    link.addEventListener("click", smoothScroll);
+
+})
